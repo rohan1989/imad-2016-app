01e49bbbd8ecff59ebadfe7e40e1265a9514dc5a
Rohan Sonawane <rsonawane13@gmail.com>
2016-09-20 00:13:18 +0530

    Removed article files

--- server.js
+++ server.js
@@ -5,6 +5,55 @@ var path = require('path');
 var app = express();
 app.use(morgan('combined'));
 
+
+var articles = {
+	'articleOne' : {
+		title: "Article One | Rohan Sonawane",
+		heading: "Article One.....",	
+	},
+	'articleTwo' : {
+		title: "Article Two | Rohan Sonawane",
+		heading: "Article Two.....",
+	},
+	'articleThree' : {
+		title: "Article Three | Rohan Sonawane",
+		heading: "Article Three.....",
+	}
+};
+
+
+
+function createTemplate(data){
+	var title = data.title;
+	var heading = data.heading;
+
+	var htmlTemplate = `
+	<!doctype html>
+	<html>
+	    <head>
+	        <title>
+	        	${title}
+	        </title>
+	        <meta name="viewport" content="width=device-width, initial-scale=1"></meta>
+	    </head>
+	    <body>
+	    	<div>
+	    		<a href="/">HOME</a>
+	    	</div>
+	    	<hr/>
+	    	<h3>
+	    		${heading}
+	    	</h3>
+	    </body>
+	</html>
+	`;
+
+	return htmlTemplate;
+
+}
+
+
+
 app.get('/', function (req, res) {
   res.sendFile(path.join(__dirname, 'ui', 'index.html'));
 });
@@ -15,6 +64,12 @@ app.get('/ui/style.css', function (req, res) {
 
 app.get('/ui/enfield.jpg', function (req, res) {
   res.sendFile(path.join(__dirname, 'ui', 'enfield.jpg'));
+});
+
+
+app.get('/:articleName', function(req, res){
+	var articleName = req.params.articleName;
+	res.send(createTemplate(articles[articleName]));
 });
 
 
