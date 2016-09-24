14620acbf6a7eef22f1ea4dd971e6199c347a340
Rohan Sonawane <rsonawane13@gmail.com>
2016-09-24 14:41:27 +0530

    handled click events

--- ui/main.js
+++ ui/main.js
@@ -1,1 +1,24 @@
-console.log('Loaded!');
+
+console.log('Loaded!..');
+
+
+var element = document.getElementById('main-text');
+console.log(element);
+
+var bulletImage = document.getElementById('bullet-img');
+
+var marginleft = 0;
+function moveRight(){
+	marginleft = marginleft + 1;
+	bulletImage.style.marginLeft = marginleft + 'px';
+}
+
+
+console.log(bulletImage);
+bulletImage.onclick = function(){
+	console.log('clicked...');
+	element.innerHTML = "Bullet Classic 350cc";
+
+	setInterval(moveRight, 10);
+};
+console.log(bulletImage.onClick);
